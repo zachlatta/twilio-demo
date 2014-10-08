57f8a0a1a77528f8e745f6dcddf86f32db30f6c4
Zach Latta <zach@zachlatta.com>
2014-10-08 09:26:49 -0700

    Twilio works.

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var bodyParser = require('body-parser');
+var twilio = require('twilio');
+
+var client = new twilio.RestClient(process.env.TWILIO_SID,
+                                   process.env.TWILIO_AUTH);
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.use(express.static('public'));
 
-app.listen(3000);
+var conns = [];
+
+app.get('/', function (req, res) {
+  res.sendfile('index.html');
+});
+
+io.on('connection', function (socket) {
+  console.log('a user connected');
+});
+
+app.post('/text', urlencodedParser, function (req, res) {
+  io.emit('txt', req.body);
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
